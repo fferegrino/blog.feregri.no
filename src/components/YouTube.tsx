e3b631040069e82d577917d9cdd785afbb4c2f12
Antonio Feregrino <antonio.feregrino@gmail.com>
2023-03-03 22:11:30 +0000

    Use TypeScript to fetch videos

--- src/components/YouTube.tsx
+++ src/components/YouTube.tsx
@@ -1,5 +1,41 @@
-import { PlayIcon } from "@heroicons/react/24/solid/index.js";
-import youtubeData from "../data/youtubeData";
+import xml2js from "xml2js";
+
+const rssFeed =
+	"https://www.youtube.com/feeds/videos.xml?channel_id=UC8KCb358oioQMcJ5pUfs8UQ";
+
+interface Video {
+	id: string;
+	published: string;
+	url: string;
+	title: string;
+	description: string;
+	thumbnail: string;
+}
+
+async function fetchVideos(): Promise<Video[]> {
+	const response = await fetch(rssFeed);
+	const xml = await response.text();
+	const result = await xml2js.parseStringPromise(xml);
+	const entries = result.feed.entry;
+
+	const videos: Video[] = [];
+	for (const entry of entries) {
+		const mediaGroup = entry["media:group"][0];
+		const video: Video = {
+			id: entry.id[0],
+			published: entry.published[0],
+			url: entry.link[0].$.href,
+			title: mediaGroup["media:title"][0],
+			description: mediaGroup["media:description"][0].split("\n")[0],
+			thumbnail: mediaGroup["media:thumbnail"][0].$.url,
+		};
+		videos.push(video);
+	}
+
+	return videos;
+}
+
+const youtubeData = await fetchVideos();
 
 const YouTube = () => {
 	return youtubeData.slice(0, 2).map((v) => (
