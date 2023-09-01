68a67716e88c4f2e7e1fdc1ece8aa4d27e0564e2
Antonio Feregrino <antonio.feregrino@gmail.com>
2023-09-01 06:21:48 +0100

    Update configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://feregri.no',
   integrations: [mdx(), sitemap(), tailwind(), react()]
 });
